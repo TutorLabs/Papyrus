f1e2dc5309adc13a636963e369f54495b21d7116
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-06-11 17:13:40 -0400

    responsive navabr

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
 import Auth from "./pages/Auth";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <main>
         <Routes>
-          <Route
-            path="/authentication"
-            element={<Auth />}
-            exact
-          ></Route>
+          <Route path="/authentication" element={<Auth />} exact></Route>
         </Routes>
       </main>
     </div>
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import { useState } from "react";
+
+function Navbar() {
+  const [isActive, setActive] = useState(false);
+
+  const toggleNavbar = () => {
+    setActive(!isActive);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="brand-title"></div>
+      <div className="toggle-button" onClick={toggleNavbar}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <div className={isActive ? "active navbar-links" : "navbar-links"}>
+        <ul>
+          <li>
+            <Link to="/authentication">Authentication</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
