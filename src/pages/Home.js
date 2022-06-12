97061a9cd7ca87dbeaa81ea785850dc98308a5ea
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-06-11 20:04:49 -0400

    home action box done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import "./Home.scss";
 
+import Create from "../images/create_posting.svg";
+
 import Header from "../components/layout/Header";
+import ActionBox from "../components/home/ActionBox";
 
 export default function Home() {
   return (
     <div className="home">
       <Header title="Home" subtitle="View information important to you" />
+      <div className="home_action_box">
+      <ActionBox img={Create} title="Posting" subtitle="Make a new posting"/>
+      <ActionBox img={Create} title="Props" subtitle="I'm passing props ok"/>
+      </div>
+     
       
     </div>
   );
