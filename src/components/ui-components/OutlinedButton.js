ce3023c7dabf966bd07d6095f2ae7a793a5ca58b
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-07-13 02:39:25 -0400

    removed comment

--- src/components/ui-components/OutlinedButton.js
+++ src/components/ui-components/OutlinedButton.js
@@ -2,10 +2,6 @@ import "./OutlinedButton.scss";
 export default function Button(prop) {
   const changeColor = {
     border: prop.green ? "1px solid #2cc3a0" : "1px solid #E23C3C",
-    // hover does not work yet
-    "&:hover": {
-      backgroundColor: prop.green ? "#2cc3a0" : "#2cc3a0",
-    },
   };
 
   return (
