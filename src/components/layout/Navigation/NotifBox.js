e294c19178d611d859a67cb52f09a76915cc9fa0
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-07-19 17:26:31 -0400

    individual notification color blue

--- src/components/layout/Navigation/NotifBox.js
+++ src/components/layout/Navigation/NotifBox.js
@@ -3,38 +3,52 @@ import "./NotifBox.scss";
 export default function NavBox() {
   return (
     <div className="notif_box">
-      <p>
-        You created a posting to teach <span>Mahzabin Rashid</span>.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
-      <p>
-        This parents <span>liked</span> you. View and apply to their posting.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
-      <p>
-        <span>Inqiyad Patwary</span> applied to teach{" "}
-        <span>Mahzabin Rashid</span>.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
-      <p>
-        You created a posting to teach <span>Mahzabin Rashid</span>.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
-      <p>
-        This parents <span>liked</span> you. View and apply to their posting.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
-      <p>
-        <span>Inqiyad Patwary</span> applied to teach{" "}
-        <span>Mahzabin Rashid</span>.
-      </p>
-      <h6>3 hours ago</h6>
-      <hr />
+      <div className="notif">
+        <p>
+          You created a posting to teach <span>Mahzabin Rashid</span>.
+        </p>
+        <h6>3 hours ago</h6>
+      </div>
+      <div className="hr_box">
+        <hr />
+      </div>
+      <div className="notif">
+        <p>
+          This parents <span>liked</span> you. View and apply to their posting.
+        </p>
+        <h6>3 hours ago</h6>
+      </div>
+      <div className="hr_box">
+        <hr />
+      </div>
+      <div className="notif">
+        <p>
+          <span>Inqiyad Patwary</span> applied to teach{" "}
+          <span>Mahzabin Rashid</span>.
+        </p>
+        <h6>3 hours ago</h6>
+      </div>
+      <div className="hr_box">
+        <hr />
+      </div>
+      <div className="notif">
+        <p>
+          You created a posting to teach <span>Mahzabin Rashid</span>.
+        </p>
+        <h6>3 hours ago</h6>
+      </div>
+      <div className="hr_box">
+        <hr />
+      </div>
+      <div className="notif">
+        <p>
+          You created a posting to teach <span>Mahzabin Rashid</span>.
+        </p>
+        <h6>3 hours ago</h6>
+      </div>
+      <div className="hr_box">
+        <hr />
+      </div>
     </div>
   );
 }
