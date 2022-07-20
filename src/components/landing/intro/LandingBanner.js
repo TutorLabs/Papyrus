6bd103e96834d92ea1e1c9accbf48ed2964ac5ba
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2022-07-20 15:34:33 -0400

    landing page banner responsive

--- src/components/landing/intro/LandingBanner.js
+++ src/components/landing/intro/LandingBanner.js
@@ -2,16 +2,20 @@ import "./LandingBanner.scss";
 
 import FirstButton from "./FirstButton";
 import SecondButton from "./SecondButton";
+import MobileBanner from "../../../images/landing_banner_mobile.png";
 
 export default function LandingBanner() {
   return (
     <div className="landing_banner">
+      <div className="mobile_banner">
+        <img  src={MobileBanner} />
+      </div>
       <h1>
         Experience <span>Better</span> <br />
         Learning.
       </h1>
       <p>
-        Find a private tutor to learn any subject you need. 
+        Find a private tutor to learn any subject you need.
         <br />
         Be a part of our community and start your new learning journey!
       </p>
