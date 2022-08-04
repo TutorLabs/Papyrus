3fac118e9c4f287b6e7c6de4e3633a38536a2144
Mahzabin Rashid <mahzabinrashidfariha@gmail.com>
2022-08-04 15:53:12 -0400

    changed /home redirect

--- src/components/edit/StudentEditContainer.js
+++ src/components/edit/StudentEditContainer.js
@@ -66,7 +66,7 @@ export default function StudentEditContainer() {
       },
       body: JSON.stringify(formData),
     }).then(() => {
-      navigate("/home");
+      navigate("/studenthome");
     });
   };
 
