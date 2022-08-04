ee36bcc3bb9b81bed53ce0bf4ce8e595f1f338af
Mahzabin Rashid <mahzabinrashidfariha@gmail.com>
2022-08-04 15:36:41 -0400

    refactored landing

--- src/components/landing/intro/LandingBanner.js
+++ src/components/landing/intro/LandingBanner.js
@@ -4,11 +4,13 @@ import FirstButton from "./FirstButton";
 import SecondButton from "./SecondButton";
 import MobileBanner from "../../../images/landing_banner_mobile.png";
 
+import { Link } from "react-router-dom";
+
 export default function LandingBanner() {
   return (
     <div className="landing_banner">
       <div className="mobile_banner">
-        <img  src={MobileBanner} />
+        <img src={MobileBanner} />
       </div>
       <h1>
         Experience <span>Better</span> <br />
@@ -20,8 +22,12 @@ export default function LandingBanner() {
         Be a part of our community and start your new learning journey!
       </p>
       <div>
-        <FirstButton />
+        <Link to="/signup" className="home_action_box_link">
+          <FirstButton />
+        </Link>
+        <Link to="/signup" className="home_action_box_link">
         <SecondButton />
+        </Link>
       </div>
     </div>
   );
